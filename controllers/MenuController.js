c972f475138ac3f8d1ff7adf65cac16e424dcaee
Brian Stones <bstones1225@hotmail.com>
2018-12-31 00:32:57 -0600

    Menu assignment

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,7 +9,8 @@
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
-          "Exit"
+          "Exit",
+          "Date"
         ]
       }
     ];
@@ -24,6 +25,8 @@
            break;
          case "Exit":
            this.exit();
+         case "Date":
+           this.getDate();
          default:
            console.log("Invalid input");
            this.main();
@@ -39,10 +42,19 @@
    }
    addContact(){
      this.clear();
-     console.log("addContact called");
+     console.log("addContact22 called");
      this.main();
    }
-
+   getDate(){
+   this.clear();
+   const d = new Date();
+   const month = d.getMonth() + 1;
+   const year = d.getFullYear();
+   const day = d.getDate();
+   console.log(month + "/"+ day + "/" + year);
+   this.main();
+   }
+   
    exit(){
      console.log("Thanks for using AddressBloc!");
      process.exit();
